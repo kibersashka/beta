d7d06185984fd21dd34efd7f293c6cebf9998ffc
kibersashka <aleksandraveldaeva4@gmail.com>
2025-08-20 20:52:47 +0500

    user and service and console

--- main/src/ru/home/booking_hairdresser/User/Service/UserService.java
+++ main/src/ru/home/booking_hairdresser/User/Service/UserService.java
@@ -1,6 +1,7 @@
 package ru.home.booking_hairdresser.User.Service;
 
 import ru.home.booking_hairdresser.User.Entity.User;
+import ru.home.booking_hairdresser.User.Repository.File.Exception.BookedTimeException;
 import ru.home.booking_hairdresser.User.Repository.UserRepository;
 
 import javax.xml.crypto.Data;
@@ -12,13 +13,13 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public void signUp(String username, String date, String time, String number) {
+    public void signUp(String username, String date, String time, String number) throws BookedTimeException {
         chekValidateDate(date, time);
         User user = new User(username, date, time, number);
         userRepository.save(user);
     }
 
-    private void chekValidateDate(String date, String time) {
+    private void chekValidateDate(String date, String time) throws BookedTimeException {
         userRepository.chekValidateDate(date, time);
     }
 
